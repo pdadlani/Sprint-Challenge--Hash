502d31391fe10ecccbc8f0189611ce8b3b297607
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-05-08 09:31:08 -0700

    finished ex3.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,30 @@
 def intersection(arrays):
 
     """
-    YOUR CODE HERE
+    create a results list and nums dictionary
+
+    iterate through all elements in the array,
+    count each elements appearance and store in dict
+
+    check if count for an element is equal to number of array within arrays
+    - if so, append that element to results list
+
+    terrible run time of O(n**2). Not proud.
     """
+
+    result = []
+    nums = dict()
+
+    for array in arrays:
+        for element in array:
+            if element in nums:
+                nums[element] += 1
+            else:
+                nums[element] = 1
+
+    for key,val in nums.items():
+        if val == len(arrays):
+            result.append(key)
 
     return result
 
